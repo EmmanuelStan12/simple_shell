8cb6c39adc9132de117d10d3b2c0cb3f17e7eb1e
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-08-12 17:06:43 +0100

    Fixed task env

--- env.c
+++ env.c
@@ -1,19 +1,6 @@
 #include "shell.h"
 
 /**
- * _print_env - prints the env variables
- * @var: nice
- * Return: the env var
- */
-void _print_env(char **argv, int i)
-{
-	if (argv[i] == NULL)
-		return;
-	_print_env(argv, i + 1);
-	printf("%s\n", argv[i]);
-}
-
-/**
  * print_env - prints the environmental variables
  * Return: void
  */
@@ -23,5 +10,12 @@ void print_env(void)
 	int i;
 
 	i = 0;
-	_print_env(env, i);
+	if (env)
+	{
+		while (env[i])
+		{
+			printf("%s\n", env[i]);
+			i++;
+		}
+	}
 }
